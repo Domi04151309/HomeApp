f4db011d71ff1ffdc043f58ea53653eb2fe099e5
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2021-11-30 18:18:13 +0100

    Update HueConnectActivity.kt

--- app/src/main/java/io/github/domi04151309/home/activities/HueConnectActivity.kt
+++ app/src/main/java/io/github/domi04151309/home/activities/HueConnectActivity.kt
@@ -3,8 +3,6 @@ package io.github.domi04151309.home.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
@@ -17,14 +15,15 @@ import io.github.domi04151309.home.custom.CustomJsonArrayRequest
 import io.github.domi04151309.home.helpers.Devices
 import io.github.domi04151309.home.helpers.Global
 import io.github.domi04151309.home.helpers.Theme
+import io.github.domi04151309.home.helpers.UpdateHandler
 import org.json.JSONObject
 
 class HueConnectActivity : AppCompatActivity() {
 
-    private var activityStarted = false
-    internal var running = false
-    internal lateinit var queue: RequestQueue
-    internal lateinit var requestToRegisterUser: CustomJsonArrayRequest
+    private val updateHandler = UpdateHandler()
+    private var success = false
+    private lateinit var queue: RequestQueue
+    private lateinit var requestToRegisterUser: CustomJsonArrayRequest
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Theme.setNoActionBar(this)
@@ -33,21 +32,20 @@ class HueConnectActivity : AppCompatActivity() {
 
         queue = Volley.newRequestQueue(this)
         val deviceId = intent.getStringExtra("deviceId") ?: ""
-        val jsonRequestObject = JSONObject("{\"devicetype\":\"HomeApp#${android.os.Build.PRODUCT}\"}")
+        val jsonRequestObject = JSONObject("{\"devicetype\":\"Home App#${android.os.Build.PRODUCT}\"}")
         requestToRegisterUser = CustomJsonArrayRequest(Request.Method.POST, Devices(this).getDeviceById(deviceId).address + "api", jsonRequestObject,
                 { response ->
                     val responseObject = response.getJSONObject(0)
                     if (responseObject.has("success")) {
-                        running = false
-                        val username = responseObject.getJSONObject("success").getString("username")
-                        PreferenceManager.getDefaultSharedPreferences(this).edit().putString(deviceId, username).apply()
-                        if (!activityStarted) {
+                        if (!success) {
+                            success = true
+                            val username = responseObject.getJSONObject("success").getString("username")
+                            PreferenceManager.getDefaultSharedPreferences(this).edit().putString(deviceId, username).apply()
                             startActivity(
-                                    Intent(this, MainActivity::class.java)
-                                            .putExtra("device", deviceId)
-                                            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                                Intent(this, MainActivity::class.java)
+                                    .putExtra("device", deviceId)
+                                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
                             )
-                            activityStarted = true
                         }
                     }
                 },
@@ -57,8 +55,6 @@ class HueConnectActivity : AppCompatActivity() {
                 }
         )
 
-        queue.add(requestToRegisterUser)
-
         findViewById<Button>(R.id.cancel_btn).setOnClickListener {
             finish()
         }
@@ -66,23 +62,13 @@ class HueConnectActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        running = true
-        startHandler()
+        updateHandler.setUpdateFunction {
+            if (!success) queue.add(requestToRegisterUser)
+        }
     }
 
     override fun onStop() {
         super.onStop()
-        running = false
-    }
-
-    private fun startHandler() {
-        val permissionHandler = Handler(Looper.getMainLooper())
-        val handlerDelay = 1000L
-        permissionHandler.postDelayed(object : Runnable {
-            override fun run() {
-                queue.add(requestToRegisterUser)
-                if (running) permissionHandler.postDelayed(this, handlerDelay)
-            }
-        }, handlerDelay)
+        updateHandler.stop()
     }
 }
