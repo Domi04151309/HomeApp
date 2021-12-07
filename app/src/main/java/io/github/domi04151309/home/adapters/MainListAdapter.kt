93fc43ddbd5d59fbd468ccf0a3ee43df9249580b
Christian Weiske <cweiske@cweiske.de>
2021-12-07 21:37:18 +0100

    Only show toggle icon when item hidden is non-empty
    .. this allows displaying non-actionable information

--- app/src/main/java/io/github/domi04151309/home/adapters/MainListAdapter.kt
+++ app/src/main/java/io/github/domi04151309/home/adapters/MainListAdapter.kt
@@ -43,7 +43,7 @@ class MainListAdapter(private val helperInterface: RecyclerViewHelperInterface) 
         holder.title.text = items[position].title
         holder.summary.text = items[position].summary
         holder.hidden.text = items[position].hidden
-        if (stateListener != null) {
+        if (stateListener != null && items[position].hidden.isNotEmpty()) {
             holder.stateSwitch.isChecked = items[position].state ?: false
             holder.stateSwitch.setOnCheckedChangeListener(stateListener)
         } else {
