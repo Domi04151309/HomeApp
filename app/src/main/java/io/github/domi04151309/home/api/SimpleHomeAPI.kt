85aa722f2f489b93f82cd5f6c81cfb04cf1de3be
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2022-01-12 11:19:33 +0100

    Add switch and none mode

--- app/src/main/java/io/github/domi04151309/home/api/SimpleHomeAPI.kt
+++ app/src/main/java/io/github/domi04151309/home/api/SimpleHomeAPI.kt
@@ -1,11 +1,13 @@
 package io.github.domi04151309.home.api
 
 import android.content.Context
+import android.util.Log
 import com.android.volley.*
 import com.android.volley.toolbox.JsonObjectRequest
 import io.github.domi04151309.home.helpers.Global.volleyError
 import io.github.domi04151309.home.R
 import io.github.domi04151309.home.data.UnifiedRequestCallback
+import io.github.domi04151309.home.helpers.Global
 import io.github.domi04151309.home.interfaces.HomeRecyclerViewHelperInterface
 
 class SimpleHomeAPI(
@@ -13,6 +15,10 @@ class SimpleHomeAPI(
     deviceId: String,
     recyclerViewInterface: HomeRecyclerViewHelperInterface?
 ) : UnifiedAPI(c, deviceId, recyclerViewInterface) {
+
+    init {
+        dynamicSummaries = false
+    }
 
     override fun loadList(callback: CallbackInterface) {
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url + "commands", null,
@@ -33,16 +39,34 @@ class SimpleHomeAPI(
     }
 
     override fun execute(path: String, callback: CallbackInterface) {
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url + path, null,
-            { response ->
-                callback.onExecuted(
-                    response.optString("toast", c.resources.getString(R.string.main_execution_completed)),
-                    response.optBoolean("refresh", false)
+        val splitCharPos = path.lastIndexOf('@')
+        val realPath = path.substring(splitCharPos + 1)
+        when (path.substring(0, splitCharPos)) {
+            "none", "switch" -> { }
+            else -> {
+                val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url + realPath, null,
+                    { response ->
+                        callback.onExecuted(
+                            response.optString("toast", c.resources.getString(R.string.main_execution_completed)),
+                            response.optBoolean("refresh", false)
+                        )
+                    },
+                    { error ->
+                        callback.onExecuted(volleyError(c, error))
+                    }
                 )
-            },
-            { error ->
-                callback.onExecuted(volleyError(c, error))
+                queue.add(jsonObjectRequest)
             }
+        }
+    }
+
+    override fun changeSwitchState(id: String, state: Boolean) {
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET,
+            url + id.substring(id.lastIndexOf('@') + 1) + "?input=" + (if (state) 1 else 0),
+            null,
+            { },
+            { e -> Log.e(Global.LOG_TAG, e.toString()) }
         )
         queue.add(jsonObjectRequest)
     }
--- app/src/main/java/io/github/domi04151309/home/api/SimpleHomeAPIParser.kt
+++ app/src/main/java/io/github/domi04151309/home/api/SimpleHomeAPIParser.kt
@@ -1,11 +1,9 @@
 package io.github.domi04151309.home.api
 
 import android.content.res.Resources
-import android.util.Log
 import io.github.domi04151309.home.R
 import io.github.domi04151309.home.data.ListViewItem
 import io.github.domi04151309.home.helpers.Global
-import org.json.JSONException
 import org.json.JSONObject
 
 class SimpleHomeAPIParser(resources: Resources) : UnifiedAPI.Parser(resources) {
@@ -14,18 +12,17 @@ class SimpleHomeAPIParser(resources: Resources) : UnifiedAPI.Parser(resources) {
         val listItems: ArrayList<ListViewItem> = ArrayList(response.length())
         val commands = response.getJSONObject("commands")
         var currentObject: JSONObject
+        var currentMode: String
         for (i in commands.keys()) {
-            try {
-                currentObject = commands.getJSONObject(i)
-                listItems += ListViewItem(
-                    title = currentObject.getString("title"),
-                    summary = currentObject.getString("summary"),
-                    hidden = i,
-                    icon = Global.getIcon(currentObject.optString("icon"), R.drawable.ic_do)
-                )
-            } catch (e: JSONException) {
-                Log.e(Global.LOG_TAG, e.toString())
-            }
+            currentObject = commands.getJSONObject(i)
+            currentMode = currentObject.optString("mode", "action")
+            listItems += ListViewItem(
+                title = currentObject.optString("title", ""),
+                summary = currentObject.optString("summary", ""),
+                hidden = "$currentMode@$i",
+                icon = Global.getIcon(currentObject.optString("icon"), R.drawable.ic_do),
+                state = if (currentMode == "switch") currentObject.optBoolean("data", false) else null
+            )
         }
         return listItems
     }
