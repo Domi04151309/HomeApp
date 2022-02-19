2a5505ca4fbb44636872cf2f72517da497307d96
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2022-02-19 18:19:23 +0100

    Attempt to update toggles

--- app/src/main/java/io/github/domi04151309/home/services/ControlService.kt
+++ app/src/main/java/io/github/domi04151309/home/services/ControlService.kt
@@ -5,8 +5,9 @@ import android.content.Intent
 import android.os.Build
 import android.service.controls.Control
 import android.service.controls.ControlsProviderService
-import android.service.controls.DeviceTypes
 import android.service.controls.actions.ControlAction
+import android.service.controls.templates.ControlButton
+import android.service.controls.templates.ToggleTemplate
 import android.util.Log
 import androidx.annotation.RequiresApi
 import io.github.domi04151309.home.R
@@ -24,6 +25,7 @@ class ControlService : ControlsProviderService() {
 
     companion object {
         private const val CONTROL_REQUEST_CODE = 1
+        private const val TAG = "ControlsBindingControllerImpl"
     }
 
     override fun createPublisherForAllAvailable(): Flow.Publisher<Control> {
@@ -51,7 +53,6 @@ class ControlService : ControlsProviderService() {
                             recyclerViewInterface: HomeRecyclerViewHelperInterface?
                         ) {
                             if (holder.response != null) {
-                                Log.wtf("ControlsBindingControllerImpl", relevantDevices[i].name)
                                 holder.response.forEach {
                                     subscriber.onNext(
                                         Control.StatelessBuilder(
@@ -74,16 +75,94 @@ class ControlService : ControlsProviderService() {
                             result: String,
                             shouldRefresh: Boolean
                         ) {
-                            finishedRequests++
-                            if (finishedRequests == relevantDevices.size) subscriber.onComplete()
                         }
                     })
             }
         }
     }
 
-    override fun createPublisherFor(controlIds: MutableList<String>): Flow.Publisher<Control> =
-        createPublisherForAllAvailable()
+    internal fun updateControls(
+        subscriber: Flow.Subscriber<in Control>,
+        controlIds: MutableList<String>,
+        controls: HashMap<String, Control>
+    ) {
+        Log.d(TAG, "sending")
+        controlIds.forEach {
+            subscriber.onNext(controls[it])
+        }
+        subscriber.onComplete()
+    }
+
+    override fun createPublisherFor(controlIds: MutableList<String>): Flow.Publisher<Control> {
+        Log.d(TAG, "publisher for $controlIds")
+        return Flow.Publisher { subscriber ->
+            val pi = PendingIntent.getActivity(
+                baseContext, CONTROL_REQUEST_CODE, Intent(),
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+            val map = HashMap<String, Control>(controlIds.size)
+            var completedRequests = 0
+            controlIds.forEach { id ->
+                val device = Devices(baseContext).getDeviceById(id.substring(0, id.indexOf('@')))
+                Global.getCorrectAPI(this, device.mode, device.id)
+                    ?.loadList(object : UnifiedAPI.CallbackInterface {
+                        override fun onItemsLoaded(
+                            holder: UnifiedRequestCallback,
+                            recyclerViewInterface: HomeRecyclerViewHelperInterface?
+                        ) {
+                            if (holder.response != null) {
+                                holder.response.forEach {
+                                    if (device.id + '@' + it.hidden == id) {
+                                        Log.d(TAG, it.toString())
+                                        val controlButton = ControlButton(true, "button")
+                                        map[id] = Control.StatefulBuilder(id, pi)
+                                            .setTitle(it.title)
+                                            .setZone(device.name)
+                                            .setStructure(resources.getString(R.string.app_name))
+                                            .setDeviceType(Global.getDeviceType(device.iconName))
+                                            .setStatus(Control.STATUS_OK)
+                                            .setControlId("button")
+                                            .setControlTemplate(
+                                                ToggleTemplate(
+                                                    "button",
+                                                    controlButton
+                                                )
+                                            )
+                                            .build()
+                                        completedRequests++
+                                        if (completedRequests == controlIds.size) updateControls(
+                                            subscriber,
+                                            controlIds,
+                                            map
+                                        )
+                                    }
+                                }
+                            } else {
+                                map[id] = Control.StatefulBuilder(id, pi)
+                                    .setTitle(device.name)
+                                    .setZone(device.name)
+                                    .setStructure(resources.getString(R.string.app_name))
+                                    .setDeviceType(Global.getDeviceType(device.iconName))
+                                    .setStatus(Control.STATUS_NOT_FOUND)
+                                    .build()
+                                completedRequests++
+                                if (completedRequests == controlIds.size) updateControls(
+                                    subscriber,
+                                    controlIds,
+                                    map
+                                )
+                            }
+                        }
+
+                        override fun onExecuted(
+                            result: String,
+                            shouldRefresh: Boolean
+                        ) {
+                        }
+                    })
+            }
+        }
+    }
 
     override fun performControlAction(p0: String, p1: ControlAction, p2: Consumer<Int>) {
         //TODO("Not yet implemented")
