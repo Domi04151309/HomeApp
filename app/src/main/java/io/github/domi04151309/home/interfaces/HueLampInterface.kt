926f1dfbcf64663da06f73812f0e39cad6b05f6c
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2022-05-17 18:48:18 +0200

    Update interfaces

---
+++ app/src/main/java/io/github/domi04151309/home/interfaces/HueLampInterface.kt
@@ -0,0 +1,12 @@
+package io.github.domi04151309.home.interfaces
+
+import io.github.domi04151309.home.data.DeviceItem
+
+interface HueLampInterface {
+    var id: String
+    var device: DeviceItem
+    var addressPrefix: String
+    var canReceiveRequest: Boolean
+
+    fun updateIconColor(color: Int)
+}
--- app/src/main/java/io/github/domi04151309/home/interfaces/HueRoomInterface.kt
+++ app/src/main/java/io/github/domi04151309/home/interfaces/HueRoomInterface.kt
@@ -1,8 +1,10 @@
 package io.github.domi04151309.home.interfaces
 
+import io.github.domi04151309.home.helpers.HueLampData
 import org.json.JSONArray
 
 interface HueRoomInterface: HueLampInterface {
     var isRoom: Boolean
     var lights: JSONArray?
+    var lampData: HueLampData
 }
