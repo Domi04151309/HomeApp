a1b859f2022b1d49b3b4efc4a70d11aab19819d6
domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2024-01-22 22:37:35 +0100

    Update detekt config

--- app/src/main/java/io/github/domi04151309/home/helpers/P.kt
+++ app/src/main/java/io/github/domi04151309/home/helpers/P.kt
@@ -3,7 +3,6 @@
 package io.github.domi04151309.home.helpers
 
 internal object P {
-    const val PREF_THEME = "theme"
     const val PREF_COLUMNS = "columns"
     const val PREF_COLUMNS_DEFAULT = "auto"
     const val PREF_CONTROLS_AUTH = "controls_auth"
