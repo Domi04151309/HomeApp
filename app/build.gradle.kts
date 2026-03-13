a267cfad24b5c45462da6ddbe611c86abfa223c5
domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2026-03-13 19:01:45 +0100

    Update java

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,10 +49,6 @@ android {
     buildFeatures {
         buildConfig = true
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
     lint {
         disable += "MissingTranslation"
     }
